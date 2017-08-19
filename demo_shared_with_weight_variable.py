fb2cf6f8faac01fe2ab8a5e13921cf738f411941
Erwan BERNARD <bernard.erwan@gmail.com>
2017-08-19 20:28:25 +0200

    add shared layer with weight

--- demo_shared_with_weight_variable.py
+++ demo_shared_with_weight_variable.py
@@ -1,52 +1,51 @@
 import logging
 import mxnet as mx
 
+logging.getLogger().setLevel(logging.DEBUG)  # logging to stdout
+
 mnist = mx.test_utils.get_mnist()
 
 batch_size = 100
 train_iter = mx.io.NDArrayIter(mnist['train_data'], mnist['train_label'], batch_size, shuffle=True)
 val_iter = mx.io.NDArrayIter(mnist['test_data'], mnist['test_label'], batch_size)
 
-# Set up variables to share parameters for all layers
-w1 = mx.sym.Variable('1_weight')
-w2 = mx.sym.Variable('2_weight')
-w3 = mx.sym.Variable('3_weight')
-b1 = mx.sym.Variable('1_bias')
-b2 = mx.sym.Variable('2_bias')
-b3 = mx.sym.Variable('3_bias')
-
 # Shared symbol 
 def get_shared_symbol(data, weight, bias):
     fc1 = mx.sym.FullyConnected(data=data, num_hidden=128, weight=weight[0], bias=bias[0])
     act1 = mx.sym.Activation(data=fc1, act_type="relu")
-    fc2 = mx.sym.FullyConnected(data=act1, num_hidden=64, weight=weight[0], bias=bias[0])
+    fc2 = mx.sym.FullyConnected(data=act1, num_hidden=64, weight=weight[1], bias=bias[1])
     act2 = mx.sym.Activation(data=fc2, act_type="relu")
-    fc3 = mx.sym.FullyConnected(data=act2, num_hidden=10, weight=weight[0], bias=bias[0])
+    fc3 = mx.sym.FullyConnected(data=act2, num_hidden=10, weight=weight[2], bias=bias[2])
     return fc3
 
+# Set up variables to share parameters for shared network
+fc_weight = []
+fc_bias = []
+for i in range(3):
+    fc_weight.append(mx.sym.Variable(str(i) + '_weight'))
+    fc_bias.append(mx.sym.Variable(str(i) + '_bias'))
 
 # Module 1 
 data = mx.sym.Variable('data')
 data = mx.sym.flatten(data=data)
-shared = get_shared_symbol(data)
+shared = get_shared_symbol(data, fc_weight, fc_bias)
 mlps = mx.sym.SoftmaxOutput(data=shared, name='softmax')
-mlp_model = mx.mod.Module(symbol=mlps, context=mx.cpu())
-mlp_model.bind(data_shapes=train_iter.provide_data, label_shapes=train_iter.provide_label)
-mlp_model.init_params()
+mlp_model1 = mx.mod.Module(symbol=mlps, context=mx.cpu())
+mlp_model1.bind(data_shapes=train_iter.provide_data, label_shapes=train_iter.provide_label)
+mlp_model1.init_params()
 
 # Module 2
 data = mx.sym.Variable('data')
 data = mx.sym.flatten(data=data)
-shared = get_shared_symbol(data)
+shared = get_shared_symbol(data, fc_weight, fc_bias)
 mlps = mx.sym.SoftmaxOutput(data=shared, name='softmax')
 mlp_model2 = mx.mod.Module(symbol=mlps, context=mx.cpu())
 mlp_model2.bind(data_shapes=train_iter.provide_data, label_shapes=train_iter.provide_label)
 mlp_model2.init_params()
 
 # Train module 1
-logging.getLogger().setLevel(logging.DEBUG)  # logging to stdout
 print("\n===Training module1===\n")
-mlp_model.fit(train_iter,  # train data
+mlp_model1.fit(train_iter,  # train data
               eval_data=val_iter,  # validation data
               optimizer='sgd',  # use SGD to train
               optimizer_params={'learning_rate': 0.1},  # use fixed learning rate
@@ -69,7 +68,7 @@ mlp_model2.fit(train_iter,  # train data
 
 # Making sure that fit doesn't always overwrite parameters by returning to module 1
 print("\n===Training module1===\n")
-mlp_model.fit(train_iter,  # train data
+mlp_model1.fit(train_iter,  # train data
               eval_data=val_iter,  # validation data
               optimizer='sgd',  # use SGD to train
               optimizer_params={'learning_rate': 0.1},  # use fixed learning rate
