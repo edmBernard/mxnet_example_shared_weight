1ae1772a7945ddc36070c50637a66b2cb82a5141
Erwan BERNARD <bernard.erwan@gmail.com>
2017-08-20 10:51:14 +0200

    add comment and clean with gluon

--- demo_with_gluon.py
+++ demo_with_gluon.py
@@ -5,7 +5,7 @@ import numpy as np
 
 ctx = mx.cpu()
 
-batch_size = 64
+batch_size = 128
 num_inputs = 784
 num_outputs = 10
 
@@ -36,9 +36,9 @@ net_mod2.collect_params().initialize(mx.init.Uniform(scale=0.1), ctx=ctx)
 
 softmax_cross_entropy = gluon.loss.SoftmaxCrossEntropyLoss()
 
-trainer_shared = gluon.Trainer(net_shared.collect_params(), 'sgd', {'learning_rate': 0.1})
-trainer_mod1 = gluon.Trainer(net_mod1.collect_params(), 'sgd', {'learning_rate': 0.1})
-trainer_mod2 = gluon.Trainer(net_mod2.collect_params(), 'sgd', {'learning_rate': 0.1})
+trainer_shared = gluon.Trainer(net_shared.collect_params(), 'sgd', {'learning_rate': 0.005})
+trainer_mod1 = gluon.Trainer(net_mod1.collect_params(), 'sgd', {'learning_rate': 0.005})
+trainer_mod2 = gluon.Trainer(net_mod2.collect_params(), 'sgd', {'learning_rate': 0.005})
 
 def evaluate_accuracy(data_iterator, net):
     acc = mx.metric.Accuracy()
@@ -54,6 +54,15 @@ epochs = 4
 moving_loss = 0.
 smoothing_constant = .01
 
+print("#### Before Training ####")
+test_accuracy = evaluate_accuracy(test_data1, lambda x: net_mod1(net_shared(x)))
+train_accuracy = evaluate_accuracy(train_data1, lambda x :net_mod1(net_shared(x)))
+print("Mod1: Train_acc %s, Test_acc %s" % (train_accuracy, test_accuracy))
+test_accuracy = evaluate_accuracy(test_data2, lambda x: net_mod2(net_shared(x)))
+train_accuracy = evaluate_accuracy(train_data2, lambda x :net_mod2(net_shared(x)))
+print("Mod2: Train_acc %s, Test_acc %s" % (train_accuracy, test_accuracy))
+
+print("#### Shared+Module1 Training ####")
 for e in range(epochs):
     # Train Branch with mod1 on dataset 1 
     for i, (data, label) in enumerate(train_data1):
@@ -66,36 +75,41 @@ for e in range(epochs):
         trainer_shared.step(batch_size)
         trainer_mod1.step(batch_size)
 
-        ##########################
-        #  Keep a moving average of the losses
-        ##########################
         curr_loss = nd.mean(loss).asscalar()
         moving_loss = (curr_loss if ((i == 0) and (e == 0))
                        else (1 - smoothing_constant) * moving_loss + (smoothing_constant) * curr_loss)
 
-    test_accuracy = evaluate_accuracy(test_data1, lambda x:net_mod1(net_shared(x)))
-    train_accuracy = evaluate_accuracy(train_data1, lambda x:net_mod1(net_shared(x)))
-    print("after1: Epoch %s. Loss: %s, Train_acc %s, Test_acc %s" % (e, moving_loss, train_accuracy, test_accuracy))
+    test_accuracy = evaluate_accuracy(test_data1, lambda x: net_mod1(net_shared(x)))
+    train_accuracy = evaluate_accuracy(train_data1, lambda x :net_mod1(net_shared(x)))
+    print("Mod1: Epoch %s. Loss: %s, Train_acc %s, Test_acc %s" % (e, moving_loss, train_accuracy, test_accuracy))
 
+# We expect the shared module to start where the first module finished
+# There will be a small accuracy decrease since one layer was not trained
+print("#### Shared+Module2 Training ####")
 for e in range(epochs):
     # Train Branch with mod2 on dataset 2 
     for i, (data, label) in enumerate(train_data2):
         data = data.as_in_context(ctx).reshape((-1,784))
         label = label.as_in_context(ctx)
         with autograd.record():
-            output = net_mod1(net_shared(data))
+            output = net_mod2(net_shared(data))
             loss = softmax_cross_entropy(output, label)
         loss.backward()
         trainer_shared.step(batch_size)
-        trainer_mod1.step(batch_size)
+        trainer_mod2.step(batch_size)
 
-        ##########################
-        #  Keep a moving average of the losses
-        ##########################
         curr_loss = nd.mean(loss).asscalar()
         moving_loss = (curr_loss if ((i == 0) and (e == 0))
                        else (1 - smoothing_constant) * moving_loss + (smoothing_constant) * curr_loss)
 
-    test_accuracy = evaluate_accuracy(test_data2, lambda x:net_mod1(net_shared(x)))
-    train_accuracy = evaluate_accuracy(train_data2, lambda x:net_mod1(net_shared(x)))
-    print("after2: Epoch %s. Loss: %s, Train_acc %s, Test_acc %s" % (e, moving_loss, train_accuracy, test_accuracy))
+    test_accuracy = evaluate_accuracy(test_data2, lambda x: net_mod2(net_shared(x)))
+    train_accuracy = evaluate_accuracy(train_data2, lambda x: net_mod2(net_shared(x)))
+    print("Mod2: Epoch %s. Loss: %s, Train_acc %s, Test_acc %s" % (e, moving_loss, train_accuracy, test_accuracy))
+
+print("#### After Training ####")
+test_accuracy = evaluate_accuracy(test_data1, lambda x: net_mod1(net_shared(x)))
+train_accuracy = evaluate_accuracy(train_data1, lambda x :net_mod1(net_shared(x)))
+print("Mod1: Train_acc %s, Test_acc %s" % (train_accuracy, test_accuracy))
+test_accuracy = evaluate_accuracy(test_data2, lambda x: net_mod2(net_shared(x)))
+train_accuracy = evaluate_accuracy(train_data2, lambda x :net_mod2(net_shared(x)))
+print("Mod2: Train_acc %s, Test_acc %s" % (train_accuracy, test_accuracy))
