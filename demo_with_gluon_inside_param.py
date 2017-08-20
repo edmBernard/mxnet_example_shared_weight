6e3c240210f8f2dccf6aec6068bc80993da8da9d
Erwan BERNARD <bernard.erwan@gmail.com>
2017-08-20 14:29:03 +0200

    fix stale parameter

--- demo_with_gluon_inside_param.py
+++ demo_with_gluon_inside_param.py
@@ -3,7 +3,6 @@ from mxnet import nd, autograd
 from mxnet import gluon
 import numpy as np
 
-@profile
 def main():
     ctx = mx.cpu()
 
@@ -19,7 +18,6 @@ def main():
     train_data2 = mx.gluon.data.DataLoader(mx.gluon.data.vision.MNIST(train=True, transform=transform), batch_size, shuffle=True)
     test_data2 = mx.gluon.data.DataLoader(mx.gluon.data.vision.MNIST(train=False, transform=transform), batch_size, shuffle=False)
 
-
     class Model(gluon.Block):
         def __init__(self, num_outputs, **kwargs):
             super(Model, self).__init__(**kwargs)
@@ -32,20 +30,20 @@ def main():
                 self.dense3_2 = gluon.nn.Dense(num_outputs)
 
         def forward(self, x, use_branch1):
-            root = nd.F.relu(self.dense1(nd.F.relu(self.dense0(x))))
+            root = nd.relu(self.dense1(nd.relu(self.dense0(x))))
             if use_branch1:
                 out = self.dense3_1(root)
             else:
                 out = self.dense3_2(root)
             return out
 
-    model = Model()
+    model = Model(num_outputs)
     model.initialize(mx.init.Uniform(scale=0.1), ctx=ctx)
 
     softmax_cross_entropy_1 = gluon.loss.SoftmaxCrossEntropyLoss()
     softmax_cross_entropy_2 = gluon.loss.SoftmaxCrossEntropyLoss()
 
-    trainer = gluon.Trainer(model, 'sgd', {'learning_rate': 0.05})
+    trainer = gluon.Trainer(model.collect_params(), 'sgd', {'learning_rate': 0.05})
 
     def evaluate_accuracy(data_iterator, net):
         acc = mx.metric.Accuracy()
@@ -79,7 +77,7 @@ def main():
                 output = model(data, True)
                 loss = softmax_cross_entropy_1(output, label)
                 loss.backward()
-            trainer.step(batch_size)
+            trainer.step(batch_size, ignore_stale_grad=True)
 
             curr_loss = nd.mean(loss).asscalar()
             moving_loss = (curr_loss if ((i == 0) and (e == 0))
@@ -102,9 +100,9 @@ def main():
             label = label.as_in_context(ctx)
             with autograd.record():
                 output = model(data, False)
-                loss = softmax_cross_entropy_1(output, label)
+                loss = softmax_cross_entropy_2(output, label)
                 loss.backward()
-            trainer.step(batch_size)
+            trainer.step(batch_size, ignore_stale_grad=True)
 
             curr_loss = nd.mean(loss).asscalar()
             moving_loss = (curr_loss if ((i == 0) and (e == 0))
