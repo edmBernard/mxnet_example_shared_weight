c1b327d62a29543618777de5e2ad7ec0dd6e0df1
Erwan BERNARD <bernard.erwan@gmail.com>
2017-08-20 15:33:51 +0200

    update metric accuracy usage

--- demo_with_gluon_one_network.py
+++ demo_with_gluon_one_network.py
@@ -34,25 +34,16 @@ def evaluate_accuracy(data_iterator, net):
         data = data.as_in_context(ctx).reshape((-1, 784))
         label = label.as_in_context(ctx)
         output = net(data)
-        predictions = nd.argmax(output, axis=1)
-        acc.update(preds=predictions, labels=label)
-    return acc.get()[1]
-
-def evaluate_accurac_v2(datas, labels, net):
-    acc = mx.metric.Accuracy()
-    for i, (data, label) in enumerate(zip(datas, labels)):
-        data = data.as_in_context(ctx).reshape((-1, 784))
-        label = label.as_in_context(ctx)
-        output = net(data)
-        predictions = nd.argmax(output, axis=1)
-        acc.update(preds=predictions, labels=label)
-    return acc.get()[1]
+        acc.update([label], [output])
+    return acc.get()
 
 epochs = 4
 moving_loss = 0.
 smoothing_constant = .01
+metric = mx.metric.Accuracy()
 
 for e in range(epochs):
+    metric.reset()
     # Train Branch with mod1 on dataset 1 
     for i, (data, label) in enumerate(train_data):
         data = data.as_in_context(ctx).reshape((-1, 784))
@@ -63,11 +54,16 @@ for e in range(epochs):
         loss.backward()
         trainer_shared.step(batch_size)
 
+        metric.update([label], [output])
+
         curr_loss = nd.mean(loss).asscalar()
         moving_loss = (curr_loss if ((i == 0) and (e == 0))
                        else (1 - smoothing_constant) * moving_loss + (smoothing_constant) * curr_loss)
-        # print("Epoch %s. Batch [%s]. Loss: %-9f, Train_acc: %s " % (e, i*batch_size, curr_loss, evaluate_accurac_v2(data, label, net_shared)))
 
-    train_accuracy = evaluate_accuracy(train_data, net_shared)
-    test_accuracy = evaluate_accuracy(test_data, net_shared)
+        if i % 100 == 0 and i > 0:
+            name, acc = metric.get()
+            print('[Epoch %d Batch %d] Loss: %s Training: %s=%f'%(e, i, moving_loss, name, acc))
+
+    _, train_accuracy = metric.get()
+    _, test_accuracy = evaluate_accuracy(test_data, net_shared)
     print("Epoch %s. Loss: %s, Train_acc %s, Test_acc %s" % (e, moving_loss, train_accuracy, test_accuracy))
