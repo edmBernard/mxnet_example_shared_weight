948b60f8f6f9600d4522efed991377b7c62107b9
Erwan BERNARD <bernard.erwan@gmail.com>
2017-08-20 14:16:54 +0200

    update shared with weight copy with a specialize layer

--- demo_shared_with_weight_copy_each_time.py
+++ demo_shared_with_weight_copy_each_time.py
@@ -15,13 +15,12 @@ def get_shared_symbol(data, weight, bias):
     act1 = mx.sym.Activation(data=fc1, act_type="relu")
     fc2 = mx.sym.FullyConnected(data=act1, num_hidden=64, weight=weight[1], bias=bias[1])
     act2 = mx.sym.Activation(data=fc2, act_type="relu")
-    fc3 = mx.sym.FullyConnected(data=act2, num_hidden=10, weight=weight[2], bias=bias[2])
-    return fc3
+    return act2
 
 # Set up variables to share parameters for shared network
 fc_weight = []
 fc_bias = []
-for i in range(3):
+for i in range(2):
     fc_weight.append(mx.sym.Variable(str(i) + '_weight'))
     fc_bias.append(mx.sym.Variable(str(i) + '_bias'))
 
@@ -29,7 +28,8 @@ for i in range(3):
 data = mx.sym.Variable('data')
 data = mx.sym.flatten(data=data)
 shared = get_shared_symbol(data, fc_weight, fc_bias)
-mlps = mx.sym.SoftmaxOutput(data=shared, name='softmax')
+fc3 = mx.sym.FullyConnected(data=shared, num_hidden=10)
+mlps = mx.sym.SoftmaxOutput(data=fc3, name='softmax')
 mlp_model1 = mx.mod.Module(symbol=mlps, context=mx.cpu())
 mlp_model1.bind(data_shapes=train_iter.provide_data, label_shapes=train_iter.provide_label)
 # mlp_model1.init_params()
@@ -38,7 +38,8 @@ mlp_model1.bind(data_shapes=train_iter.provide_data, label_shapes=train_iter.pro
 data = mx.sym.Variable('data')
 data = mx.sym.flatten(data=data)
 shared = get_shared_symbol(data, fc_weight, fc_bias)
-mlps = mx.sym.SoftmaxOutput(data=shared, name='softmax')
+fc3 = mx.sym.FullyConnected(data=shared, num_hidden=10)
+mlps = mx.sym.SoftmaxOutput(data=fc3, name='softmax')
 mlp_model2 = mx.mod.Module(symbol=mlps, context=mx.cpu())
 mlp_model2.bind(data_shapes=train_iter.provide_data, label_shapes=train_iter.provide_label)
 # mlp_model2.init_params()
@@ -51,7 +52,8 @@ mlp_model1.fit(train_iter,  # train data
               optimizer_params={'learning_rate': 0.1},  # use fixed learning rate
               eval_metric='acc',  # report accuracy during training
               batch_end_callback=mx.callback.Speedometer(batch_size, 100),
-              num_epoch=5)  # train for at most 10 dataset passes
+              num_epoch=5,
+              allow_missing=True)  # train for at most 10 dataset passes
 
 
 # Train module 2
@@ -65,7 +67,8 @@ mlp_model2.fit(train_iter,  # train data
                eval_metric='acc',  # report accuracy during training
                batch_end_callback=mx.callback.Speedometer(batch_size, 100),
                num_epoch=5,
-               arg_params=arg_param)  # train for at most 10 dataset passes
+               arg_params=arg_param,
+               allow_missing=True)  # train for at most 10 dataset passes
 
 
 # Making sure that fit doesn't always overwrite parameters by returning to module 1
@@ -78,4 +81,5 @@ mlp_model1.fit(train_iter,  # train data
               eval_metric='acc',  # report accuracy during training
               batch_end_callback=mx.callback.Speedometer(batch_size, 100),
               num_epoch=5,
-              arg_params=arg_param)  # train for at most 10 dataset passes
+              arg_params=arg_param,
+              allow_missing=True)  # train for at most 10 dataset passes
